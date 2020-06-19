12e55c499cc7c51dac96c5a8db2dfb187f249f75
Harry Coa <rodol_2000@hotmail.com>
2020-06-18 20:41:02 -0500

    relacionar modelo con el input del form

--- src/app/components/outcome/outcome-create/outcome-create.component.ts
+++ src/app/components/outcome/outcome-create/outcome-create.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { OutcomeCreateModel } from './models/outcome-create.model';
+import { UserStorageService } from '../../../services/user-storage.service';
 
 @Component({
   selector: 'app-outcome-create',
   templateUrl: './outcome-create.component.html',
-  styleUrls: ['./outcome-create.component.css']
+  styleUrls: ['./outcome-create.component.css'],
 })
 export class OutcomeCreateComponent implements OnInit {
-
-  constructor() { }
+  public model: OutcomeCreateModel = new OutcomeCreateModel();
+  constructor(public userStorageService: UserStorageService) {}
 
   ngOnInit(): void {
+    this.model.userId = this.userStorageService.user.id;
   }
 
+  onSubmit(): void {
+    console.log(this.model);
+  }
 }
