8896c5602d40972536364aa1963d6e599f8b6722
Harry Coa <rodol_2000@hotmail.com>
2020-08-26 15:21:11 -0500

    update

--- src/app/components/outcome/outcome-update/outcome-update.component.ts
+++ src/app/components/outcome/outcome-update/outcome-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { OutcomeService } from 'src/app/services/outcome.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { OutcomeUpdateModel } from 'src/app/services/models/outcome-update.model';
 
 @Component({
   selector: 'app-outcome-update',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OutcomeUpdateComponent implements OnInit {
 
-  constructor() { }
+  public model: OutcomeUpdateModel = new OutcomeUpdateModel();
+  public loading: boolean = false;
+
+  constructor(
+    private outcomeService: OutcomeService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loading = true;
+    this.outcomeService.get(this.route.snapshot.params.id)
+      .subscribe((data) => {
+        this.model = data;
+        this.loading = false;
+      });
+  }
+
+  onSubmit(): void {
+    this.loading = true;
+
+    this.outcomeService.update(this.route.snapshot.params.id, this.model)
+      .subscribe(() => {
+        this.router.navigate(['/outcomes']);
+      });
   }
 
 }
--- src/app/services/outcome.service.ts
+++ src/app/services/outcome.service.ts
@@ -4,6 +4,7 @@ import { OutcomeModel } from '../components/login/models/outcome.model';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { OutcomeCreateModel } from '../services/models/outcome-create.model';
+import { OutcomeUpdateModel } from './models/outcome-update.model';
 
 @Injectable({
   providedIn: 'root',
@@ -24,5 +25,17 @@ export class OutcomeService {
 
   create(model: OutcomeCreateModel): Observable<{}>{
     return this.http.post(`${environment.apiUrl}outcomes`, model);
+  }
+
+  get(id: string): Observable<OutcomeModel> {
+    return this.http.get<OutcomeModel>(`${environment.apiUrl}outcomes/${id}`);
+  }
+
+  delete(id: string): Observable<{}> {
+    return this.http.delete(`${environment.apiUrl}outcomes/${id}`);
+  }
+
+  update(id: string, model: OutcomeUpdateModel): Observable<{}> {
+    return this.http.put(`${environment.apiUrl}outcomes/${id}`, model);
   }
 }
